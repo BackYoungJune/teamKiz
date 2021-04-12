a4594f225730c334392301e32a45c09c511078e0
BackYoungJune <74597441+BackYoungJune@users.noreply.github.com>
2021-04-12 14:37:26 +0900

    youngJune

---
+++ Team portfolio/Assets/Script/yCenter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yCenter : MonoBehaviour
+{
+    Vector3 ScreenCenter = Vector3.zero;
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        ScreenCenter = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+    }
+}
