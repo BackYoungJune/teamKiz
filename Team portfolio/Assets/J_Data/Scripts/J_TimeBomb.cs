aa0c7a011a2bc349f5c427858e275b0fdf60140b
ZUUUUUUB <zuuuuuub@gmail.com>
2021-04-19 12:49:47 +0900

    ExplosionPrefab

---
+++ Team portfolio/Assets/J_Data/Scripts/J_TimeBomb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_TimeBomb : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+    float timer = 10.0f;
+    bool IsPlanted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlantBomb();
+    }
+
+    void PlantBomb()
+    {
+        if (IsPlanted) return;
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(SetTimer());
+        }
+    }
+
+    IEnumerator SetTimer()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            // 남은 시간 표시 
+        }
+        else
+        {
+            Explosion();
+        }
+        yield return null;
+    }
+
+    void Explosion()
+    {
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+    }
+}
