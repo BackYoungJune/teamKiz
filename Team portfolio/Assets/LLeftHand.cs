76a0cc9dd06ea15a8a06e2c4bf9f69b2ce82b9c2
kg1220 <73523249+kg1220@users.noreply.github.com>
2021-04-19 12:08:14 +0900

    revert

---
+++ Team portfolio/Assets/LLeftHand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LLeftHand : MonoBehaviour
+{
+    public LBossAnimEvent myAnimEvent;
+    public float Damage = 10.0f;
+    private void Awake()
+    {
+        myAnimEvent.AttackColliderOn += () =>
+         {
+             this.GetComponent<CapsuleCollider>().enabled = true;
+         };
+        myAnimEvent.AttackColliderOff += () =>
+        {
+            this.GetComponent<CapsuleCollider>().enabled = false;
+        };
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            other.GetComponent<yPlayerHealth>().OnDamage(Damage, other.ClosestPoint(transform.position), transform.position - other.transform.position);
+        }
+    }
+}
