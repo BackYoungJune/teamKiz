5f8dbb8c1641f4840640d0bd4460faecef11e0aa
BackYoungJune <74597441+BackYoungJune@users.noreply.github.com>
2021-04-23 00:49:17 +0900

    YoungJune
    CameraMove

---
+++ Team portfolio/Assets/Script/yCameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yCameraMove : MonoBehaviour
+{
+    Animator CameraAnim;
+    yPlayerInput Input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraAnim = GetComponent<Animator>();
+        Input = FindObjectOfType<yPlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.aim)
+        {
+            CameraAnim.SetBool("aim", true);
+        }
+        else
+        {
+            CameraAnim.SetBool("aim", false);
+        }
+    }
+}
