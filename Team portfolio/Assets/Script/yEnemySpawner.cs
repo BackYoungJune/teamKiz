9d63b427376bae2afd9a07026603fc5217acf3f1
hotddul <80609072+manaoflibya@users.noreply.github.com>
2021-04-26 14:07:22 +0900

    4/26(2)

--- Team portfolio/Assets/Script/yEnemySpawner.cs
+++ Team portfolio/Assets/Script/yEnemySpawner.cs
@@ -95,6 +95,7 @@ public class yEnemySpawner : MonoBehaviour
         // 적 사망 시 점수 상승
         enemy.onDeath += () => yGameManager.instance.AddScore((int)score);
         /* 좀비 죽었을 경우 유석 UI 추가하기*/
+        //enemy.onDeath += () => 
     }
 
     Transform WaveSpawn(int wave)
