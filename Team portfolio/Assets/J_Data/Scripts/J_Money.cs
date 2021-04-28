e29a25272d509a4319ff1741a0015c11041fc89b
MJ <zuuuuuub@gmail.com>
2021-04-28 09:39:06 +0900

    MJ

---
+++ Team portfolio/Assets/J_Data/Scripts/J_Money.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Money : MonoBehaviour
+{
+    public J_Item item;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            J_ItemManager.instance.remainMoney += item.restore;
+            Destroy(gameObject);
+        }
+    }
+}
