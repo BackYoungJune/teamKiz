2311aab70c7f3b7ecb846362d604ce33c7051540
MJ <zuuuuuub@gmail.com>
2021-05-01 13:29:25 +0900

    Money Sound

--- Team portfolio/Assets/J_Data/Scripts/J_Money.cs
+++ Team portfolio/Assets/J_Data/Scripts/J_Money.cs
@@ -6,12 +6,28 @@ public class J_Money : MonoBehaviour
 {
     public J_Item item;
 
+    public AudioSource audioSource;
+    public AudioClip moneySound;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            J_ItemManager.instance.remainMoney += item.restore;
-            Destroy(gameObject);
+            StartCoroutine(GetMoney());
         }
+    }
+
+    IEnumerator GetMoney()
+    {
+        Sound.I.PlayEffectSound(moneySound, audioSource);
+        J_ItemManager.instance.remainMoney += item.restore;
+
+        yield return new WaitForSeconds(0.6f);
+        Destroy(gameObject);
     }
 }
